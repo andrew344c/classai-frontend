06b1a27e26b9783fbf8bb88bb4a46052d91de872
Pattur <42484929+Pattur@users.noreply.github.com>
2020-08-19 19:37:31 -0700

    Updated SubmitAssignment

--- src/components/Classroom/SubmitAssignmentDialog.js
+++ src/components/Classroom/SubmitAssignmentDialog.js
@@ -4,14 +4,15 @@ import { connect } from "react-redux";
 import { uploadSubmission } from "../../redux/actions/userActions";
 import { getSubmissions } from "../../redux/actions/dataActions";
 
-import { Dialog, Button, Paper, Typography } from "@material-ui/core";
+import { Dialog, Button, Paper, Typography, Grid } from "@material-ui/core";
+import Alert from '@material-ui/lab/Alert';
 import { withStyles } from "@material-ui/styles";
 
-import "./SubmitAssignmentDialog.scss";
 
 const styles = (theme) => ({
     paper: {
         marginBottom: 20,
+        width: "100%"
     },
     img: {
         width: "100%",
@@ -29,20 +30,33 @@ class SubmitAssignmentDialog extends Component {
 
     onFileChange = (event) => {
         this.setState({ selectedFile: event.target.files[0] });
+        document.getElementById('upload').innerHTML = "File: " + event.target.files[0].name;
     };
 
     onFileSubmit = () => {
         const formData = new FormData();
-        formData.append(
-            "submission",
-            this.state.selectedFile,
-            this.state.selectedFile.name
-        );
-        this.props.uploadSubmission(
-            formData,
-            this.props.classroomId,
-            this.props.assignmentId
-        );
+        if(this.state.selectedFile != null) {
+	        formData.append(
+	            "submission",
+	            this.state.selectedFile,
+	            this.state.selectedFile.name
+	        );
+	        this.props.uploadSubmission(
+	            formData,
+	            this.props.classroomId,
+	            this.props.assignmentId
+	        );
+	        document.getElementById('right').style.display = "flex";
+	        setTimeout(function() {
+	        	document.getElementById('right').style.display = "none";
+	        }, 3000);
+	    }
+	    else {
+	    	document.getElementById('notright').style.display = "flex";
+	    	setTimeout(function() {
+	        	document.getElementById('notright').style.display = "none";
+	        }, 3000);
+	    }
     };
 
     getSubmissions = () => {
@@ -51,17 +65,17 @@ class SubmitAssignmentDialog extends Component {
             this.props.assignmentId
         );
     };
-
     render() {
         const { classes } = this.props;
 
         return (
-            <Dialog open={this.props.open} onClose={this.props.onClickAway}>
+            <div style={{width: "100%"}} open={this.props.open} onClose={this.props.onClickAway}>
+                <div style={{display: "none"}}>
                 <button onClick={this.getSubmissions}>Get submissions</button>
                 {this.props.submissions.map((submission) => {
                     console.log(submission);
                     return (
-                        <Paper className={classes.paper}>
+                    	<Paper className={classes.paper}>
                             <Typography variant="h4">{`${submission.creator.firstName} ${submission.creator.lastName}`}</Typography>
                             <img
                                 src={submission.downloadUrl}
@@ -84,12 +98,7 @@ class SubmitAssignmentDialog extends Component {
                     />
 
                     <label htmlFor="file-upload" id="file-drag">
-                        <img
-                            id="file-image"
-                            src="#"
-                            alt="Preview"
-                            className="hidden"
-                        />
+              
                         <div id="start">
                             <i
                                 className="fa fa-download"
@@ -119,7 +128,48 @@ class SubmitAssignmentDialog extends Component {
                     </label>
                     <Button onClick={this.onFileSubmit}>Submit</Button>
                 </form>
-            </Dialog>
+                </div>
+            <Dialog open={this.props.open} onClose={this.props.onClickAway} contentStyle={{width: "100%", maxWidth: "none"}}>
+                <div className={classes.root} style={{margin: 0, padding: 0, backgroundColor: "#e5e5e5", padding: "5%"}}>
+                <Alert id="notright" style={{display: "none"}} variant="outlined" severity="error">Please upload a file before you submit</Alert>
+                <Alert id="right" style={{display: "none"}} variant="outlined" severity="success">Your file has been submitted</Alert>
+			      <Grid container spacing={3}>
+				  	<Grid container item xs={12} spacing={3}>
+				    	<Grid item xs={12}>
+				          <p style={{textAlign: "center", fontFamily: "Roboto"}}><strong>SUBMIT YOUR WORK HERE</strong></p>
+				        </Grid>
+				        <Grid item xs={12}>
+				        	<Button id="upload" onClick={openDialog} variant="contained" color="primary" style={{width: "100%"}}>+ Upload Files</Button>
+				        </Grid>
+				        <Grid item xs={6}>
+				          <Button onClick={this.onFileSubmit} variant="contained" color="primary" style={{width: "100%"}}>Submit</Button>
+				        </Grid>
+				        <Grid item xs={6}>
+				          <Button onClick={this.getSubmissions} variant="contained" color="primary" style={{width: "100%"}}>Get Submissions</Button>
+				        </Grid>
+				      </Grid>
+				      <Grid item xs={12} spacing = {3}>
+			      		{this.props.submissions.map((submission) => {
+	                    console.log(submission);
+	                    return (
+	                    	<Paper className={classes.paper}>
+	                            <Typography variant="h4">{`${submission.creator.firstName} ${submission.creator.lastName}`}</Typography>
+	                            <img
+	                                src={submission.downloadUrl}
+	                                key={submission.id}
+	                                className={classes.img}
+	                                alt="submission"
+	                            />
+	                            <Typography>{`Text Derived From OCR: ${submission.text}`}</Typography>
+	                            <Typography>{`Similar Websites to Submission: ${submission.plagarismLinks}`}</Typography>
+	                        </Paper>
+	                    	);
+	                	})}
+			      </Grid>
+			      </Grid>
+			    </div>
+			  </Dialog>
+			</div>
         );
     }
 }
@@ -138,3 +188,7 @@ export default connect(
     mapStateToProps,
     mapActionToProps
 )(withStyles(styles)(SubmitAssignmentDialog));
+
+function openDialog() {
+  document.getElementById('file-upload').click();
+}
