a84d61d3b34abcb764996a6936cd01c9dbd902dc
Andrew <andrew344c@gmail.com>
2020-10-15 01:17:14 -0700

    Profile Settings more responsive and add functionality to textfields

--- src/components/Settings/ProfileSettings.js
+++ src/components/Settings/ProfileSettings.js
@@ -15,44 +15,71 @@ import {
     Typography,
     Button,
     TextField,
+    IconButton,
 } from "@material-ui/core";
 
 export default class ProfileSettings extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            loggingOut: false,
+            firstName: "",
+            lastName: "",
+            description: "",
         };
     }
+
+    onChange = (event) => {
+        event.persist();
+        event.preventDefault();
+
+        this.setState((oldState) => ({
+            ...oldState,
+            [event.target.name]: event.target.value,
+        }));
+    };
+
+    onSubmit = () => {
+        console.log(this.state);
+    };
+
     render() {
-        const { username, firstName, lastName } = this.props;
+        const username = localStorage.getItem("username");
+        const firstName = localStorage.getItem("firstName");
+        const lastName = localStorage.getItem("lastName");
 
         return (
             <div>
                 <div class="row settings">
-                    <div class="col-4" style={{ marginTop: "20px" }}>
+                    <div class="col-lg-4 col-12" style={{ marginTop: "20px" }}>
                         <h2>Profile Picture</h2>
                         <div
                             class="d-flex align-items-center"
                             style={{ marginTop: "20px" }}
                         >
-                            <Avatar
-                                style={{
-                                    width: "120px",
-                                    height: "120px",
-                                }}
-                                src={tempImg}
+                            <IconButton>
+                                <Avatar
+                                    style={{
+                                        width: "120px",
+                                        height: "120px",
+                                    }}
+                                    src={tempImg}
+                                />
+                            </IconButton>
+                            <ListItemText
+                                style={{ paddingLeft: "2em" }}
+                                primary={`${firstName} ${lastName}`}
+                                secondary={username}
+                                primaryTypographyProps={{ variant: "h5" }}
                             />
-                            <Typography style={{marginLeft: "0.6em"}}variant="h4">Andrew Chiang</Typography>
                         </div>
                     </div>
                     <div
-                        class="col-8 d-flex align-items-center"
+                        class="col-lg-8 col-12 d-flex align-items-center"
                         style={{ marginTop: "20px" }}
                     >
                         <div class="row">
                             <div class="col-12">
-                                <h2>Names</h2>
+                                <h2>Change Names</h2>
                             </div>
                             <div class="col-6">
                                 <TextField
@@ -60,7 +87,9 @@ export default class ProfileSettings extends Component {
                                         width: "100%",
                                     }}
                                     id="standard-basic"
-                                    label={firstName}
+                                    label="First Name"
+                                    name="firstName"
+                                    onChange={this.onChange}
                                 />
                             </div>
                             <div class="col-6">
@@ -69,25 +98,10 @@ export default class ProfileSettings extends Component {
                                         width: "100%",
                                     }}
                                     id="standard-basic"
-                                    label={lastName}
+                                    label="Last Name"
+                                    name="lastName"
+                                    onChange={this.onChange}
                                 />
-                            </div>
-                            <div class="col-12">
-                                <TextField
-                                    style={{
-                                        width: "100%",
-                                    }}
-                                    id="standard-basic"
-                                    label={username}
-                                />
-                            </div>
-                            <div class="col-12">
-                                <button
-                                    class="btn btn-primary btn-block"
-                                    style={{ marginTop: "30px" }}
-                                >
-                                    Submit Changes
-                                </button>
                             </div>
                         </div>
                     </div>
@@ -97,14 +111,25 @@ export default class ProfileSettings extends Component {
                             id="standard-multiline-flexible"
                             multiline
                             rows={10}
-                            value="insert description here (no description variable yet"
+                            name="description"
+                            onChange={this.onChange}
+                            value={this.state.description}
+                            fullWidth
                             style={{
-                                width: "100%",
                                 border: "1px solid #d8d8d8",
                                 padding: "10px",
                                 borderRadius: "10px",
                             }}
                         />
+                    </div>
+                    <div class="col-12">
+                        <button
+                            class="btn btn-primary btn-block"
+                            style={{ marginTop: "30px" }}
+                            onClick={this.onSubmit}
+                        >
+                            Submit Changes
+                        </button>
                     </div>
                 </div>
             </div>
