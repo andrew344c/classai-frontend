db1db3c25fea514c95620370703bbd9b39d53a1f
Pattur <42484929+Pattur@users.noreply.github.com>
2020-10-14 22:43:27 -0700

    Update Grades.js

--- src/pages/Grades.js
+++ src/pages/Grades.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 
-import Settings from "../components/Settings/Settings";
 import GradesList from "../components/Grades/GradesList";
 import Navbar from '../components/Navigation/Navbar';
 
@@ -10,7 +9,6 @@ export default class Grades extends Component {
             <div>
                 <Navbar />
                 <GradesList />
-                <Settings />
             </div>
         )
     }
